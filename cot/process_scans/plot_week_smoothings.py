71c0c2452e02b63ad2d03e053d3664b6d4c925ce
Aturnal <thomas.hendriks@student.uantwerpen.be>
2017-05-15 18:27:04 +0200

    Replaced code duplication with an import stmt.
    Former-commit-id: d0a92d1d4771baeaea967839437be473e0976469 [formerly 19cab2e8ff5389f7ce89699988455d209ae27cb8 [formerly 19cab2e8ff5389f7ce89699988455d209ae27cb8 [formerly 94f753179a5a64e639c89b7514eb9eef7f61955a]]]
    Former-commit-id: 6f5910a716f46e8a7c4755f05d37baa928523b77
    Former-commit-id: 35688990b4f6e6ce52d3259304d95069c24f8235
    Former-commit-id: 6632583b99e9111363df5a7da1b5fc8a546d099e

--- cot/process_scans/plot_week_smoothings.py
+++ cot/process_scans/plot_week_smoothings.py
@@ -4,88 +4,13 @@ import sys
 
 from freq_channelnr_map import map_ as channelnr_map
 
+from plot_rssi import savitzky_golay as savitzky_golay
+
 counter = 0
 maxes = []
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
-
-
-### REMOVE SECONDS FROM FILENAMES WITH
-### ls -d ??????????????????? | xargs rename 's/.{3}$//'
-
-def savitzky_golay(y, window_size, order, deriv=0, rate=1):
-    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-    The Savitzky-Golay filter removes high frequency noise from data.
-    It has the advantage of preserving the original shape and
-    features of the signal better than other types of filtering
-    approaches, such as moving averages techniques.
-    Parameters
-    ----------
-    y : array_like, shape (N,)
-        the values of the time history of the signal.
-    window_size : int
-        the length of the window. Must be an odd integer number.
-    order : int
-        the order of the polynomial used in the filtering.
-        Must be less then `window_size` - 1.
-    deriv: int
-        the order of the derivative to compute (default = 0 means only smoothing)
-    Returns
-    -------
-    ys : ndarray, shape (N)
-        the smoothed signal (or it's n-th derivative).
-    Notes
-    -----
-    The Savitzky-Golay is a type of low-pass filter, particularly
-    suited for smoothing noisy data. The main idea behind this
-    approach is to make for each point a least-square fit with a
-    polynomial of high order over a odd-sized window centered at
-    the point.
-    Examples
-    --------
-    t = np.linspace(-4, 4, 500)
-    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-    ysg = savitzky_golay(y, window_size=31, order=4)
-    import matplotlib.pyplot as plt
-    plt.plot(t, y, label='Noisy signal')
-    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-    plt.plot(t, ysg, 'r', label='Filtered signal')
-    plt.legend()
-    plt.show()
-    References
-    ----------
-    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-       Data by Simplified Least Squares Procedures. Analytical
-       Chemistry, 1964, 36 (8), pp 1627-1639.
-    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-       Cambridge University Press ISBN-13: 9780521880688
-    """
-    import numpy as np
-    from math import factorial
-
-    try:
-        window_size = np.abs(np.int(window_size))
-        order = np.abs(np.int(order))
-    except ValueError, msg:
-        raise ValueError("window_size and order have to be of type int")
-    if window_size % 2 != 1 or window_size < 1:
-        raise TypeError("window_size size must be a positive odd number")
-    if window_size < order + 2:
-        raise TypeError("window_size is too small for the polynomials order")
-    order_range = range(order + 1)
-    half_window = (window_size - 1) // 2
-    # precompute coefficients
-    b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
-    m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
-    # pad the signal at the extremes with
-    # values taken from the signal itself
-    firstvals = y[0] - np.abs(y[1:half_window + 1][::-1] - y[0])
-    lastvals = y[-1] + np.abs(y[-half_window - 1:-1][::-1] - y[-1])
-    y = np.concatenate((firstvals, y, lastvals))
-    return np.convolve(m[::-1], y, mode='valid')
 
 
 def process_point(day, hour, minute):
@@ -107,7 +32,7 @@ if __name__ == '__main__':
     if (len(sys.argv) < 6):
         print "Usage: python plot_rssi.py dump_directory comma_separated_list_of_days first_minute filter_num channel_number [prev_day next_day]\n\
                Example: To plot for all channels on 2,3,4 May 2017, first dump at 2 minutes past the hour ignoring the top 100 results for every dump: \n\
-               python plot_week_smoothings.py ../output_dir 2017-05-02,2017-05-03,2017-05-04 2 100 0 2017-05-01 2017-05-05\
+               python plot_week_smoothings.py ../output_dir 2017-05-02,2017-05-03,2017-05-04 0 100 0 2017-05-01 2017-05-05\n\
                Note that the channel number is required: 0 means all channels\n\
                To use previous and next day's last and first hour to avoid potentially ugly results at end points.\n\
                These are only used in calculations and not actually plotted."
