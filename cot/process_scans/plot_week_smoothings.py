0fa7f240c8d0b508a27348baaa99589091edf8e3
Aturnal <thomas.hendriks@student.uantwerpen.be>
2017-05-25 19:59:01 +0200

    Some changes, generating 'clean' graphs.
    Former-commit-id: 1a7bc6e8a19fbc705126c6a923c77dfe79a505c4 [formerly 11f514391f8bc341adfd4bb3e392ce7ee29cea38 [formerly 11f514391f8bc341adfd4bb3e392ce7ee29cea38 [formerly 7782d86b59ae16b6435afe35218fa06979b4e7ea]]]
    Former-commit-id: c52c1e1111a597ff5b1e15846dc7f35aabc778db
    Former-commit-id: 2d549bddc4add11ed890aabea0324df394549664
    Former-commit-id: 40e696ebd3d6c1259f82db2c3636103d06c30320

--- cot/process_scans/plot_week_smoothings.py
+++ cot/process_scans/plot_week_smoothings.py
@@ -6,6 +6,8 @@ from freq_channelnr_map import map_ as channelnr_map
 
 from plot_rssi import savitzky_golay as savitzky_golay
 
+#rename 's/...$//' *
+
 counter = 0
 maxes = []
 import numpy as np
@@ -17,6 +19,7 @@ def process_point(day, hour, minute):
     signals = []  # Fill with every nf+rssi of dump
     file_name = directory + ('/' if not directory.endswith('/') else '') + day + '_' + str(hour).zfill(2) + '-' + str(
         minute).zfill(2)
+
     signalstr = subprocess.check_output(
         ['./fft_get_max_rssi.out', file_name]) if not channel_number else subprocess.check_output(
         ['./fft_get_max_rssi.out', file_name, channelnr_map[channel_number]])
@@ -24,15 +27,22 @@ def process_point(day, hour, minute):
         signals.append(int(line))
     signals.sort()
     signals.reverse()
+
+    if (len(signals) == 0):
+        print(file_name)
+        print ""
     return signals[filter_num]
 
 
 if __name__ == '__main__':
-    colors = ["blue","green","red","cyan","magenta","yellow","black","white"]
+    if (len(sys.argv) < 9 and len(sys.argv) >= 8) :
+        print("Please also supply node name at the end of command")
+    print(sys.argv[8])
+    colors = ["blue","green","red","cyan","magenta","yellow","black","0.75", "#788599", "#2c8e36", "#7f0043"]
     if (len(sys.argv) < 6):
         print "Usage: python plot_rssi.py dump_directory comma_separated_list_of_days first_minute filter_num channel_number [prev_day next_day]\n\
                Example: To plot for all channels on 2,3,4 May 2017, first dump at 2 minutes past the hour ignoring the top 100 results for every dump: \n\
-               python plot_week_smoothings.py ../output_dir 2017-05-02,2017-05-03,2017-05-04 0 100 0 2017-05-01 2017-05-05\n\
+               python plot_week_smoothings.py ../output_dir 2017-05-02,2017-05-03,2017-05-04 0 100 0 2017-05-01 2017-05-05 node_name\n\
                Note that the channel number is required: 0 means all channels\n\
                To use previous and next day's last and first hour to avoid potentially ugly results at end points.\n\
                These are only used in calculations and not actually plotted."
@@ -70,8 +80,8 @@ if __name__ == '__main__':
         maxes = []
 
         if fix_endpoints:
-            for (minute) in range(first_minute, 60, 2):
-                max_point = process_point(prev_day, 23, minute)
+            for (hour, minute) in itertools.product(range(20,24), range(first_minute, 60, 2)):
+                max_point = process_point(prev_day, hour, minute)
                 maxes.append(max_point)
 
         for (hour, minute) in itertools.product(range(24), range(first_minute, 60, 2)):
@@ -79,42 +89,43 @@ if __name__ == '__main__':
             maxes.append(max_point)
 
         if fix_endpoints:
-            for (minute) in range(first_minute, 60, 2):
-                max_point = process_point(next_day, 0, minute)
+            for (hour, minute) in itertools.product(range(0,4), range(first_minute, 60, 2)):
+                max_point = process_point(next_day, hour, minute)
                 maxes.append(max_point)
         daylist[i].append(maxes)
 
-    x = np.linspace(0, 24, 720) if not fix_endpoints else np.linspace(-1, 25, 780)
+    x = np.linspace(0, 24, 720) if not fix_endpoints else np.linspace(-4, 28, 960)
 
     label_list = []
 
     for day in daylist:
         y = np.asarray(day[3])
-        day.append(savitzky_golay(y, 101, 3))   #Index 4
+        # day.append(savitzky_golay(y, 101, 3))   #Index 4
+        day.append("None")
         day.append(savitzky_golay(y, 361, 3))   #Index 5
         day.append(colors[0])
         day.append(colors[0])
         colors.remove(colors[0])
 
-        sm1 = plt.plot(x, day[4], color=day[6])
+        #sm1 = plt.plot(x, day[4], color=day[6])
         sm2 = plt.plot(x, day[5], color=day[7])
 
-        label_list.append(mpatches.Patch(color=day[6], label=day[0]+ ', order 3, window size 101'))
+        label_list.append(mpatches.Patch(color=day[6], label=day[0]+ ', order 3, window size 361'))
 
 
     # Ticks at even hours
     plt.xticks(np.arange(0, 25, 2))
     # Cut off endpoints fix; only show current day
     plt.xlim([0, 24])
-    plt.xlabel('Hours')
-    plt.ylabel('Received signal strength (dBm)')
+    plt.xlabel('Time of Day (hours)')
+    plt.ylabel('Smoothed Received Signal Strength (dBm)')
 
     #Additional 15dBm on top for legend
     plt.ylim([plt.gca().get_ylim()[0],plt.gca().get_ylim()[1]+15])
 
-    plt.legend(handles=label_list)
+    #plt.legend(handles=label_list,prop={'size':5})
 
-    # plt.savefig(day[0]+"_image.png", bbox_inches='tight')
-    # plt.savefig(day[0]+"_image.pdf", bbox_inches='tight')
+    plt.savefig("./images/channel_specific_2.4/"+sys.argv[8]+"_"+day[0]+"_"+"chan"+str(channel_number)+"_image.png", bbox_inches='tight')
+    plt.savefig("./images/channel_specific_2.4/"+sys.argv[8]+"_"+day[0]+"_"+"chan"+str(channel_number)+"_image.pdf", bbox_inches='tight')
 
-    plt.show()
+    # plt.show()
