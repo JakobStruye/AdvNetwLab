576cc7f9360f71fde9f478de6af2915c07d57657
Jakob Struye <jakob.struye@student.uantwerpen.be>
2017-06-30 18:56:17 +0200

    Removed username and server name from flush_files scripts #forJohan

--- #ScriptsForJohan/flush_files.sh
+++ #ScriptsForJohan/flush_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no  -r output s0139858@student.mosaic.uantwerpen.be:~/out/$(hostname) || exit 1
+scp -o StrictHostKeyChecking=no  -r output {username}@{server}.be:~/out/$(hostname) || exit 1
 rm output/*
